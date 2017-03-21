ef5c20a4ed015aae0a74a86395f5f61774aa1218
xiaojian.chen <xiaojian.chen@mobvista.com>
2017-03-21 13:30:23 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+
 from time import sleep
 from weibo import APIClient
 import json
@@ -5,20 +6,6 @@ import io
 from selenium import webdriver
 import ConfigParser
 from selenium.webdriver.common.keys import Keys
-
-cf = ConfigParser.ConfigParser()
-cf.read("config.ini")
-APP_KEY = cf.get('weibo','APP_KEY')
-APP_SECRET = cf.get('weibo','APP_SECRET')
-CALLBACK_URL = cf.get('weibo','CALLBACK_URL')
-USERID = cf.get('weibo','USERID')
-USERPASSWD = cf.get('weibo','USERPASSWD')
-
-driver = webdriver.PhantomJS(executable_path='/home/cxj/Downloads/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-
-client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-url = client.get_authorize_url()
-# print url
 
 def getCurrentUrl(url):
     driver.get(url)
@@ -51,15 +38,30 @@ def getData(code):
 
     client.set_access_token(access_token, expires_in)
 
-    res = client.statuses.public_timeline.get(count=2)
+    # res = client.statuses.public_timeline.get(count=2)
     # res  = client.statuses.user_timeline.get(uid=3937348351)
     # res=client.emotions.get()
     # res = client.friendships.friends.get(uid=3937348351)
-    # res = client.users.show.get(uid=5407847973)
+    res = client.users.show.get(uid=5407847973)
     return res
 
+cf = ConfigParser.ConfigParser()
+cf.read("config.ini")
+APP_KEY = cf.get('weibo','APP_KEY')
+APP_SECRET = cf.get('weibo','APP_SECRET')
+CALLBACK_URL = cf.get('weibo','CALLBACK_URL')
+USERID = cf.get('weibo','USERID')
+USERPASSWD = cf.get('weibo','USERPASSWD')
+
+driver = webdriver.PhantomJS(executable_path='/home/cxj/Downloads/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+
+client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
+url = client.get_authorize_url()
+# print url
+
 code = getCode(url)
+print 'code:'+code
+
 data = getData(code)
 outPut(data)
-print 'code:'+code
 driver.quit()
